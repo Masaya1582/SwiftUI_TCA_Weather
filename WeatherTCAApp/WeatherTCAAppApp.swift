323c5618f13e0a1e5be8b197b1e4b01d08f0cc57
Masaya Nakakuki <masa8y88@gmail.com>
2025-09-01 14:54:38 +0900

    Initial Commit

---
+++ WeatherTCAApp/WeatherTCAAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherTCAAppApp.swift
+//  WeatherTCAApp
+//
+//  Created by MasayaNakakuki on 2025/09/01.
+//
+
+import SwiftUI
+
+@main
+struct WeatherTCAAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WeatherTCAAppTests/WeatherTCAAppTests.swift
@@ -0,0 +1,16 @@
+//
+//  WeatherTCAAppTests.swift
+//  WeatherTCAAppTests
+//
+//  Created by MasayaNakakuki on 2025/09/01.
+//
+
+import Testing
+
+struct WeatherTCAAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
